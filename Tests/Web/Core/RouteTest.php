e7e448db4e6d02369d61359255d38c974fe9f3e2
Giacomello Nathan <nathan.giacomello@smals.be>
2019-03-01 14:17:44 +0100

    add tests for Route class

---
+++ Tests/Web/Core/RouteTest.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: nagi
+ * Date: 01/03/2019
+ * Time: 14:01
+ */
+
+namespace Tests\Web\Core;
+
+
+use PHPUnit\Framework\TestCase;
+use Web\Core\Route;
+
+class RouteTest extends TestCase {
+
+    function testRoute() {
+        // / ==> TestController
+        Route::connect("/", "Test");
+        self::assertEquals([
+            "controller" => "Test",
+            "param" => ""
+        ], Route::getRoute("/"));
+        self::assertEquals([
+            "controller" => "Test",
+            "param" => "/a"
+        ], Route::getRoute("/a"));
+        self::assertEquals([
+            "controller" => "Test",
+            "param" => "/a/a"
+        ], Route::getRoute("/a/a"));
+        self::assertEquals([
+            "controller" => "Test",
+            "param" => "/a/a/a/A/E/F"
+        ], Route::getRoute("/a/a/a/A/E/F"));
+
+        Route::connect("/user", "User");
+        self::assertEquals([
+            "controller" => "Test",
+            "param" => ""
+        ], Route::getRoute("/"));
+        self::assertEquals([
+            "controller" => "Test",
+            "param" => "/use"
+        ], Route::getRoute("/use"));
+        self::assertEquals([
+            "controller" => "User",
+            "param" => ""
+        ], Route::getRoute("/user"));
+        // Uppercase
+        self::assertEquals([
+            "controller" => "User",
+            "param" => "/F"
+        ], Route::getRoute("/USER/F"));
+    }
+}
